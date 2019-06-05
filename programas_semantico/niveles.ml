99a762aabc8d7548ee08782aa27676153dd6a05b
DESKTOP-6V36O5F\Gord <737215@unizar.es>
2019-06-05 23:10:12 +0200

    Ultimo programita semantico

---
+++ programas_semantico/niveles.ml
@@ -0,0 +1,57 @@
+programa niveles;
+	entero i;
+	accion o;
+	entero j;
+		accion p;	
+			entero k;		
+			accion q;			
+				entero l;				
+				accion r;
+					entero m;
+					principio
+						i := i + 1; 
+						j := j + 1; 
+						k := k + 1; 
+						l := l + 1; 
+						m := 5;
+						escribir (i, j, k, l, m);
+						r; 
+						q; 
+						p; 
+						o;
+					fin
+				principio
+					i := i + 1; 
+					j := j + 1; 
+					k := k + 1; 
+					l := 4;
+					r; 
+					q; 
+					p; 
+					o;
+				fin
+			principio
+			i := i + 1; 
+			j := j + 1; 
+			k := 3;
+			q; 
+			p; 
+			o;
+		fin
+	principio
+	i := i + 1; 
+	j := 2; 
+	p; 
+	o;
+	fin
+principio
+	i := 1;
+	o
+fin
+
+
+
+
+
+
+
