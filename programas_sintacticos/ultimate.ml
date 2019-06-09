609083cc36ecae12231439b820b3481bc260a6b7
DESKTOP-6V36O5F\Gord <737215@unizar.es>
2019-06-09 21:58:39 +0200

    fin de las pruebas

---
+++ programas_sintacticos/ultimate.ml
@@ -0,0 +1,24 @@
+programa ultimate
+	ciudadano i,j k;
+	caracter 124;
+	entero true;
+
+accion ();
+principio
+fin
+	
+accion foo2(entero a1);
+fin
+
+accion "cocinar" (ref);
+principio
+	a123 := a546 * 7;
+fin
+
+
+
+
+
+
+
+
